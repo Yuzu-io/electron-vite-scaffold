4366fccfe4b9eee0bbb5a7cd123ee0147d3ddc34
Yuzu <1581523609@qq.com>
2023-07-18 22:43:41 +0800

    feat:初始化css样式

--- src/renderer/auth/main.ts
+++ src/renderer/auth/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './plugins/router';
 
+// 初始化样式
+import '../../../resources/reset.css';
+
 const app = createApp(App);
 
 app.use(router);
